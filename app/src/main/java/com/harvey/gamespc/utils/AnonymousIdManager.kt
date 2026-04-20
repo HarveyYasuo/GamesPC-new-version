66b372aa446369f17c8d1f440b23f229f66488e2
HarveyYasuo <harveyrivas66@gmail.com>
2026-04-20 13:06:57 -0500

    fix: convertir AnonymousIdManager a clase inyectable y renombrar método para consistencia

--- app/src/main/java/com/harvey/gamespc/utils/AnonymousIdManager.kt
+++ app/src/main/java/com/harvey/gamespc/utils/AnonymousIdManager.kt
@@ -2,24 +2,29 @@ package com.harvey.gamespc.utils
 
 import android.content.Context
 import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.UUID
+import javax.inject.Inject
+import javax.inject.Singleton
 
-object AnonymousIdManager {
-
+@Singleton
+class AnonymousIdManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
     private const val PREFS_NAME = "AppPrefs"
     private const val KEY_UNIQUE_ID = "unique_id"
 
-    private fun getPreferences(context: Context): SharedPreferences {
+    private fun getPreferences(): SharedPreferences {
         return context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
     }
 
-    fun getUniqueId(context: Context): String {
-        val prefs = getPreferences(context)
+    fun getAnonymousId(): String {
+        val prefs = getPreferences()
         var uniqueId = prefs.getString(KEY_UNIQUE_ID, null)
         if (uniqueId == null) {
             uniqueId = UUID.randomUUID().toString()
             prefs.edit().putString(KEY_UNIQUE_ID, uniqueId).apply()
         }
-        return uniqueId
+        return uniqueId!!
     }
 }
