850105468c957a920c7b442a416b79e7f6a4552c
HarveyYasuo <harveyrivas66@gmail.com>
2026-04-21 09:13:59 -0500

    fix: añadir import de Settings icon

--- app/src/main/java/com/harvey/gamespc/ui/MainScreen.kt
+++ app/src/main/java/com/harvey/gamespc/ui/MainScreen.kt
@@ -16,6 +16,8 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
