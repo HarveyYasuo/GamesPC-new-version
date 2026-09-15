70abca2fcc2e372b546c3fab9989728f3f8a96e1
HarveyYasuo <harveyrivas66@gmail.com>
2026-09-15 09:12:50 -0500

    chore: subir versión a 1.1.8 (63)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.harvey.gamespc"
         minSdk = 23
         targetSdk = 36
-        versionCode = 62
-        versionName = "1.1.7"
+        versionCode = 63
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
